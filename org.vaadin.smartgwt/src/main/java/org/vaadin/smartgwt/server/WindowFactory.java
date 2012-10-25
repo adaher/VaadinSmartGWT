f6c6648d6606caac080269dc968ef0a7cbf2b7f1
Antoine Daher <adaher@360-innovations.com>
2012-10-25 17:05:56 -0400

    Added Modal Window method to WindowFactory

--- org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/server/WindowFactory.java
+++ org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/server/WindowFactory.java
@@ -2,17 +2,26 @@ package org.vaadin.smartgwt.server;
 
 import org.vaadin.smartgwt.server.layout.MasterContainer;
 
-public class WindowFactory
-{
+public class WindowFactory {
 	private final MasterContainer masterContainer;
 
-	public WindowFactory(MasterContainer masterContainer)
-	{
+	public WindowFactory(MasterContainer masterContainer) {
 		this.masterContainer = masterContainer;
 	}
 
-	public Window newWindow()
-	{
+	public Window newWindow() {
 		return new Window(masterContainer);
+	}
+
+	public Window newModalWindow() {
+		Window window = new Window(masterContainer);
+		window.setCanDragResize(true);
+		window.setAutoCenter(true);
+		window.setShowMinimizeButton(false);
+		window.setShowMaximizeButton(false);
+		window.setIsModal(true);
+		window.setShowModalMask(true);
+		window.setAutoSize(true);
+		return window;
 	}
 }
